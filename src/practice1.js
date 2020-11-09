b4bb117624f667e13026c38eb62804fee5336702
g992987642 <992987642@qq.com>
2020-11-09 23:15:12 +0800

    [guhao]:finished all problem

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,17 @@
-const parseData
-export { parseData };
+const parseData = (resource) => {
+  const {data, column} = resource;
+  const keys = column.map(c => c.name)
+  return data.map(values => createObject(keys, values))
+};
+
+const createObject = (keys, values)=> {
+  const obj = {};
+  keys.forEach((key, i) => {
+	obj[key] = values[i];
+  });
+  return obj;
+};
+
+
+
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
-export { inject };
+const inject = (items, sections) => {
+  const result = [];
+  items.forEach((key, i) => {
+	const section = sections.find(({index}) => index == i);
+	if(section){
+	  result.push(section.content)
+	}
+	result.push(key);
+  });
+  return result;
+}
+export {inject};
